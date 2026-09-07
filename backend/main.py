44c9771cb27d1fa923512816fd3fd87815849f87
savy32729-code <savy32729@gmail.com>
2026-09-07 22:07:59 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,7 @@ import os
 import shutil
 import uuid
 from pathlib import Path
-BASE_DIR = 
-Path(__file__).resolve().parent
+BASE_DIR = Path(file).resolve().parent
 AUDIO_DIR = BASE_DIR / "audio"
 
 AUDIO_DIR.mkdir(parents=True, exist_ok=True)
